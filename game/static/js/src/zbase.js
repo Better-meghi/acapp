59df8553e65f3f991f0cd50947470da7c81992c9
Better-meghi <3366955689@qq.com>
2023-11-19 14:16:00 +0800

    first step

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,9 @@
 class AcGame {
     constructor(id) {
+        console.log("creat ac game");
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
     }
 }
 
--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -4,5 +4,8 @@ JS_PATH=/home/lzy/acapp/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
-
 find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+
+
+
+
