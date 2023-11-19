d85400615523caf45508b8fc58cfd269b5290794
Better-meghi <3366955689@qq.com>
2023-11-19 20:29:37 +0800

    html page finished

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,9 +1,13 @@
 class AcGame {
     constructor(id) {
-        console.log("creat ac game");
         this.id = id;
         this.$ac_game = $('#' + id);
         this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
+
+        this.start();
+    }
+    start(){
     }
 }
 
