7e1e648e296ecb7ec3fc84d201d580932a890324
Better-meghi <3366955689@qq.com>
2023-11-23 18:07:16 +0800

    debug finish

--- game/static/js/src/playground/particle/zbase.js
+++ game/static/js/src/playground/particle/zbase.js
@@ -1,12 +1,13 @@
 class Particle extends AcGameObject {
     constructor(playground,x, y, radius, vx, vy, color, speed) {
         super();
-        this.playground =this.playground;
+        this.playground = playground;
         this.ctx = this.playground.game_map.ctx;
         this.x = x;
         this.y = y;
         this.vx = vx;
         this.vy = vy;
+        this.radius = radius;
         this.color = color;
         this.speed = speed;
         this.friction = 0.9;
